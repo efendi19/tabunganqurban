db08df1d8171524011fb6f5390a26d51a890df94
efendi19 <efen.ndi19@gmail.com>
2019-08-29 23:41:15 +0700

    UI Login

---
+++ app/src/main/java/com/efendiyt/tabunganqurban/ui/login/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.efendiyt.tabunganqurban.ui.login;
+
+import android.app.Activity;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.efendiyt.tabunganqurban.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+  //  private LoginViewModel loginViewModel;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final EditText usernameEditText = findViewById(R.id.username);
+        final EditText passwordEditText = findViewById(R.id.password);
+        final Button loginButton = findViewById(R.id.login);
+      //  final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+
+    }
+}
